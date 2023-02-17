ff3a947d8b8ccd3ee151373f25d0dd6302a23e46
Rohit Singh <11rohitchs@gmail.com>
2023-02-18 00:46:26 +0530

    change in sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum {
         public static void main(String[] args) {
             int a=5;
             int b=6;
-            System.out.println("the sum is="+a+b);
+            System.out.println("the sum is==="+a+b);
         }
      }
 
